151b22edf506faf3bb2d1cdced60dede69ba66e3
Dmytro-Horbachov <d.p.gorbachov@gmail.com>
2024-05-20 21:25:00 +0300

    dz07-v001

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+const mainLiElem = document.querySelectorAll('ul#categories > .item');
+console.log(`Number of categories: ${mainLiElem.length}`);
+
+const allTitle = mainLiElem.forEach(elem => {
+  const titleName = elem.querySelector('h2').textContent;
+  const allItems = elem.querySelectorAll('li').length;
+  console.log(`Category: ${titleName}`);
+  console.log(`Elements: ${allItems}`);
+});
